2a288ee40f258d26d711f2da3c2350588258057d
JangWonLee <wonnna1021@gmail.com>
2014-07-22 18:05:43 +0900

    project UPDATE

--- baseballtext/src/net/ib/baseballtext/MainActivity.java
+++ baseballtext/src/net/ib/baseballtext/MainActivity.java
@@ -1,32 +1,52 @@
 package net.ib.baseballtext;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import kr.co.quizon.network.HttpLib;
 import kr.co.quizon.network.link.match.GetMatchBroadcastLink;
+import kr.co.quizon.network.link.match.GetMatchListLink;
 import net.ib.quizon.api.match.GetMatchBroadcastReq;
 import net.ib.quizon.api.match.GetMatchBroadcastRes;
+import net.ib.quizon.api.match.GetMatchListReq;
+import net.ib.quizon.api.match.GetMatchListRes;
+import net.ib.quizon.domain.match.Match;
 import net.ib.quizon.domain.match.MatchBroadcast;
 import net.ib.quizon.domain.match.MatchDisplayBoard;
 import net.ib.quizon.domain.match.MatchPlayers;
+import net.ib.quizon.domain.match.MatchStatus;
 import net.ib.quizon.domain.match.MatchSummary;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 	private final String ACCESS_TOKEN = "b867b048-8f20-4a01-bfc4-53784e4b488e";
+	private Integer INNING; // ?????????????????????/  null이면????? 
+	ArrayList<String> matchList;
+	
+	
     private TextView baseballText;
-    private TextView teamName1;
-    private TextView teamName2;
+    private TextView homeTeamNameText;
+    private TextView awayTeamNameText;
+    private TextView stadiumText;
+    private TextView inningText;
+    private TextView homeTeamPointText;
+    private TextView awayTeamPointText;
+    private TextView ballText;
+    private TextView strikeText;
+    private TextView outText;
+    
     private Button button;
     
-    private String matchId = "7580b1a4-ac96-4369-a93e-f462be19df88";
+    private String matchId;// = "1f3f46ec-e01a-47e2-a54e-b1371d0d4806";
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,63 +56,82 @@ public class MainActivity extends Activity implements OnClickListener {
         baseballText = (TextView) findViewById(R.id.baseballtext);
         baseballText.setMovementMethod(new ScrollingMovementMethod());
         
-        teamName1 = (TextView) findViewById(R.id.teamname1);
-        teamName2 = (TextView) findViewById(R.id.teamname2);
+        homeTeamNameText = (TextView) findViewById(R.id.hometeamname);
+        awayTeamNameText = (TextView) findViewById(R.id.awayteamname);
+        stadiumText = (TextView) findViewById(R.id.stadium);
+        inningText = (TextView) findViewById(R.id.inning);
+        homeTeamPointText = (TextView) findViewById(R.id.hometeampoint);
+        awayTeamPointText = (TextView) findViewById(R.id.awayteampoint);
+        ballText = (TextView) findViewById(R.id.ball);
+        strikeText = (TextView) findViewById(R.id.strike);
+        outText = (TextView) findViewById(R.id.out);
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
+        
+        matchList = new ArrayList<String>();
+        matchList.add("");
+        matchList.add("");
+        
+        
     }
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		HttpLib.setTest(true);
-//		setData();
+		setData();
+	}
+
+	private void setData() {
+		new AsyncTask<Void, Void, List<Match>>() {
+			@Override
+			protected List<Match> doInBackground(Void... params) {
+				// network process
+				GetMatchListReq getMatchListReq = new GetMatchListReq();
+				getMatchListReq.setAccessToken(ACCESS_TOKEN);
+				GetMatchListLink getMatchListLink = new GetMatchListLink(getMatchListReq);
+				GetMatchListRes matchListRes = getMatchListLink.linkage();
+				if (matchListRes != null) {
+					return matchListRes.getMatchInfoList();
+				}
+				return null;
+			}
+
+			@Override
+			protected void onPostExecute(List<Match> result) {
+				// ui process
+				if (result != null) {
+					for (Match match : result) {
+						match.getMatchStatus();
+						
+						 matchId = match.getMatchId();
+						if (matchId != null) {
+							baseballText.setText("매치 ID : " + matchId);
+							
+//							break;
+						}
+					}
+				} else {
+					baseballText.setText("매치 정보 로딩 실패");
+				}
+			}
+		}.execute();
 	}
 	
-//	private void setData() {
-//		new AsyncTask<Void, Void, List<Match>>() {
-//			@Override
-//			protected List<Match> doInBackground(Void... params) {
-//				// network process
-//				GetMatchListReq getMatchListReq = new GetMatchListReq();
-//				getMatchListReq.setAccessToken(accessToken);
-//				GetMatchListLink getMatchListLink = new GetMatchListLink(getMatchListReq);
-//				GetMatchListRes matchListRes = getMatchListLink.linkage();
-//				if (matchListRes != null) {
-//					return matchListRes.getMatchInfoList();
-//				}
-//				return null;
-//			}
-//
-//			@Override
-//			protected void onPostExecute(List<Match> result) {
-//				// ui process
-//				if (result != null) {
-//					for (Match match : result) {
-////						matchId = match.getMatchId();
-//						if (matchId != null) {							
-//							textView.setText("매치 ID : " + matchId);
-//							break;
-//						}
-//					}
-//				} else {
-//					textView.setText("매치 정보 로딩 실패");
-//				}
-//			}
-//		}.execute();
-//	}
-	
-	private void getBroadcast(String matchId, String inning) {
+	private void getBroadcast(String matchId, final String inning) {
+		INNING = Integer.parseInt(inning);
 		new AsyncTask<String, Void, GetMatchBroadcastRes>() {
 			@Override
 			protected GetMatchBroadcastRes doInBackground(String... params) {
 				GetMatchBroadcastReq getMatchBroadcastReq = new GetMatchBroadcastReq();
 				getMatchBroadcastReq.setMatchId(params[0]);
-				getMatchBroadcastReq.setInning(Integer.valueOf(params[1]));
+				if (inning != null) {
+					getMatchBroadcastReq.setInning(Integer.valueOf(inning));
+				}
 				GetMatchBroadcastLink getMatchBroadcastLink = new GetMatchBroadcastLink(getMatchBroadcastReq);
 				GetMatchBroadcastRes getMatchBroadcastRes = getMatchBroadcastLink.linkage();
 				if (getMatchBroadcastRes != null) {
-					return getMatchBroadcastRes;
+					return getMatchBroadcastRes; 
 				}
 				return null;
 			}
@@ -100,11 +139,12 @@ public class MainActivity extends Activity implements OnClickListener {
 			@Override
 			protected void onPostExecute(GetMatchBroadcastRes result) {
 				if (result != null) {
-//					StringBuilder sb = new StringBuilder();
-//					sb.append("매치 정보");
-//					sb.append("\n");
-//					sb.append(result);
-//					sb.append("\n");
+//					Log.i("test", result.toString());
+					StringBuilder sb = new StringBuilder();
+					sb.append("매치 정보");
+					sb.append("\n");
+					sb.append(result);
+					sb.append("\n");
 					
 					List<MatchBroadcast> broadcast = result.getBroadcast();
 					for (MatchBroadcast matchBroadcast : broadcast) {
@@ -113,14 +153,20 @@ public class MainActivity extends Activity implements OnClickListener {
 						}
 						String broadcast2 = matchBroadcast.getBroadcast();
 						Integer inning2 = matchBroadcast.getInning();
+//						inning = matchBroadcast.getInning();
 					}
+//					result.getMatchSummaryList().
+//					MatchDetail.this.getMatch().getMatchStadium();
+					
 					MatchDisplayBoard matchDisplayBoard = result.getMatchDisplayBoard();
 					setMatchDisplayBoard(matchDisplayBoard);
 					
 					MatchPlayers matchPlayers = result.getMatchPlayers();
 					setMatchPlayers(matchPlayers);
 					
+					
 					List<MatchSummary> matchSummaryList = result.getMatchSummaryList();
+					
 					for (MatchSummary matchSummary : matchSummaryList) {
 						if (matchSummary == null) {
 							continue;
@@ -135,14 +181,22 @@ public class MainActivity extends Activity implements OnClickListener {
 						String matchPresent = matchSummary.getMatchPresent();
 						String matchStadium = matchSummary.getMatchStadium();
 						Integer matchStatus = matchSummary.getMatchStatus();
+						
+						stadiumText.setText(matchStadium);
+						inningText.setText(matchPresent);
 					}
+//					MatchSummary matchSummary = null;
+//					String matchStadium = matchSummary.getMatchStadium();s
+//					stadiumText.setText(m)
 					
-//					baseballText.setText(sb.toString());
+					
+					
+					baseballText.setText(sb.toString());
 				} else {
 					baseballText.setText("문자 중계 로딩 실패");
 				}
 			}
-		}.execute(matchId, inning);
+		}.execute(matchId);
 	}
 
 	@Override
@@ -150,7 +204,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		switch (v.getId()) {
 		case R.id.button:
 			if (matchId != null) {				
-				getBroadcast(matchId, String.valueOf(1));
+				getBroadcast(matchId, String.valueOf(4));
 			}
 			break;
 		default:
@@ -161,20 +215,29 @@ public class MainActivity extends Activity implements OnClickListener {
 	private void setMatchDisplayBoard(MatchDisplayBoard matchDisplayBoard) {
 		if (matchDisplayBoard != null) {
 			String awayTeamName = matchDisplayBoard.getAwayTeamName();
-//			sb.append("AwayTeamName");
-//			sb.append("\t");
-//			sb.append(awayTeamName);
-//			sb.append("\n");
+			if (awayTeamName != null) {				
+				awayTeamNameText.setText(awayTeamName);
+			}
 			List<String> awayTeamPoint = matchDisplayBoard.getAwayTeamPoint();
-			String ball = matchDisplayBoard.getBall();
+			awayTeamPointText.setText(awayTeamPoint.get(INNING-1));
+			
 			String homeTeamName = matchDisplayBoard.getHomeTeamName();
-//			sb.append("HomeTeamName");
-//			sb.append("\t");
-//			sb.append(homeTeamName);
-//			sb.append("\n");
+			if (homeTeamName != null) {				
+				homeTeamNameText.setText(homeTeamName);
+			}
 			List<String> homeTeamPoint = matchDisplayBoard.getHomeTeamPoint();
-			String out = matchDisplayBoard.getOut();
+			
+//			homeTeamPointText.setText(homeTeamPoint.get(homeTeamPoint.size()-1));
+			homeTeamPointText.setText(homeTeamPoint.get(INNING-1));
+			
+			
+//			뭔가이상해..
+			String ball = matchDisplayBoard.getBall();
+			ballText.setText(ball);
 			String strike = matchDisplayBoard.getStrike();
+			strikeText.setText(strike);
+			String out = matchDisplayBoard.getOut();
+			outText.setText(out);
 		}
 	}
 	
